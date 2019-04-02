e520986f17835d984d933baa678735c74a802563
BeniBV <35697924+BeniBV@users.noreply.github.com>
2019-04-02 16:30:58 +0100

    Loading Assets

---
+++ BlackManorDLC/Assets/Scripts/CharacterMovementScripts/CharacterController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    CharacterInput character_input;
+    Rigidbody mybody; 
+
+    public float Speed;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character_input = GetComponent<CharacterInput>();
+        mybody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(horizontal, 0f, vertical) * Speed * Time.deltaTime;
+        transform.Translate(moveDirection);
+
+    }
+}
---
+++ BlackManorDLC/Assets/Scripts/Interactables/DoorOpen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+
+    public float TheDistance;
+    public GameObject ActionDisplay;
+    public GameObject Actiontext;
+    public GameObject TheDoor;
+
+    public AudioSource CreakSound; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        TheDistance = PlayerCasting.DistanceFromTarget;
+    }
+
+    private void OnMouseOver()
+    {
+        if (TheDistance <= 3)
+        {
+            ActionDisplay.SetActive(true);
+            Actiontext.SetActive(true);
+
+        }
+        if (Input.GetButtonDown("Action"))
+        {
+            if (TheDistance <= 3)
+            {
+                this.GetComponent<BoxCollider>().enabled = false;
+                ActionDisplay.SetActive(false);
+                Actiontext.SetActive(false);
+                TheDoor.GetComponent<Animation>().Play("FirstDoorOpenAnim");
+                CreakSound.Play();
+            }
+        }
+    }
+     void OnMouseExit()
+
+    {
+        ActionDisplay.SetActive(false);
+        Actiontext.SetActive(false);
+    }
+}
+
