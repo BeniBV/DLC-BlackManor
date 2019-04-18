6f8a3e89999bc01a2eb6b9d87c73a2c6f595bcd0
BeniBV <35697924+BeniBV@users.noreply.github.com>
2019-04-18 15:01:14 +0100

    Adding health hearts

---
+++ BlackManorDLC/Assets/Scripts/PlayerStats/HealthHearts.cs
@@ -0,0 +1,32 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthHearts : MonoBehaviour
+{
+    public int health;
+    public int numberOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    private void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < numberOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+            }
+        }
+    }
+}
+*/ 
